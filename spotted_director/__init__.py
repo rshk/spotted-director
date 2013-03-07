63d0459c7cd135ce7f701659b7e0a9ea8297993c
Samuele Santi <redshadow@hackzine.org>
2013-03-07 02:48:42 +0100

    More work on the js ui. Now supporting listing the messages on server.

--- spotted_director/__init__.py
+++ spotted_director/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, json, make_response
 
 app = Flask(__name__)
 
@@ -18,141 +18,177 @@ def get_server_connection(server_name):
 def dashboard(path=None):
     return render_template('index.jinja')
 
-
-@app.route('/server')
-def server_list():
-    return render_template(
-        'server-list.jinja',
-        servers=app.config['SERVERS'])
-
-
-@app.route('/server/<server_name>', methods=['GET', 'POST'])
-def server_dashboard(server_name):
-    client = get_server_connection(server_name)
-    messages = client.list_messages()
-
-    return render_template(
-        'server-dashboard.jinja',
-        name=server_name,
-        host=app.config['SERVERS'][server_name],
-        messages=messages)
-
-
-@app.route('/server/<server_name>/add')
-def server_add_message(server_name):
-    return render_template(
-        'message-add.jinja',
-        name=server_name,
-        host=app.config['SERVERS'][server_name])
-
-
-@app.route('/server/<server_name>/action', methods=['POST'])
-def server_action(server_name):
-    client = get_server_connection(server_name)
-
-    if request.method == 'POST':
-        action = request.form['action']
-
-        if action == 'add':
-            message = request.form['message']
-            client.add_message(message)
-
-        elif action == 'delete':
-            message_id = request.form['message_id']
-            client.delete_message(message_id)
-
-        else:
-            raise ValueError("Unknown action %s" % action)
-
-    return redirect(url_for('server_dashboard', server_name=server_name))
-
-
+# @app.route('/server')
+# def server_list():
+#     return render_template(
+#         'server-list.jinja',
+#         servers=app.config['SERVERS'])
+#
+#
+# @app.route('/server/<server_name>', methods=['GET', 'POST'])
+# def server_dashboard(server_name):
+#     client = get_server_connection(server_name)
+#     messages = client.list_messages()
+#
+#     return render_template(
+#         'server-dashboard.jinja',
+#         name=server_name,
+#         host=app.config['SERVERS'][server_name],
+#         messages=messages)
+#
+#
+# @app.route('/server/<server_name>/add')
+# def server_add_message(server_name):
+#     return render_template(
+#         'message-add.jinja',
+#         name=server_name,
+#         host=app.config['SERVERS'][server_name])
+#
+#
+# @app.route('/server/<server_name>/action', methods=['POST'])
+# def server_action(server_name):
+#     client = get_server_connection(server_name)
+#
+#     if request.method == 'POST':
+#         action = request.form['action']
+#
+#         if action == 'add':
+#             message = request.form['message']
+#             client.add_message(message)
+#
+#         elif action == 'delete':
+#             message_id = request.form['message_id']
+#             client.delete_message(message_id)
+#
+#         else:
+#             raise ValueError("Unknown action %s" % action)
+#
+#     return redirect(url_for('server_dashboard', server_name=server_name))
 
 ## ===== API views =============================================================
 ## Objects: server, screen_message(server), queue_message
 ## URLs:
 
-##      GET /server/
-##          List servers
+def _json_response(data):
+    response = make_response(json.dumps(data))
+    response.headers['Content-type'] = 'application/json'
+    return response
+
+## GET /server/
+## List servers
 @app.route('/api/server', methods=['GET'])
 def api_get_server():
-    pass
+    servers = []
+    for server_id, server in app.config['SERVERS'].iteritems():
+        servers.append({
+            'id': server_id,
+            'url': server,
+        })
+    return _json_response(servers)
 
-##      POST /server/
-##          Add a server connection
+## POST /server/
+## Add a server connection
 @app.route('/api/server', methods=['POST'])
 def api_post_server():
-    pass
+    assert request.content_type == 'application/json'
+    data = json.loads(request.data)
+    assert 'url' in data
+    server_id = data['id'] if 'id' in data else data['url']
+    assert server_id not in app.config['SERVERS']
+    app.config['SERVERS'][server_id] = data['url']
 
-##      GET /server/<id>
-##          Get info on a server connection
+## GET /server/<id>
+## Get info on a server connection
 @app.route('/api/server/<server_id>', methods=['GET'])
 def api_get_server_id(server_id):
-    pass
+    server = app.config['SERVERS'][server_id]
+    return _json_response({
+        'id': server_id,
+        'url': server,
+    })
 
-##      PUT /server/<id>
+## GET /server/<id>/status
+## Get server status
+@app.route('/api/server/<server_id>/status', methods=['GET'])
+def api_get_server_id_status(server_id):
+    client = get_server_connection(server_id)
+    return _json_response(client.server_info())
+
+## PUT /server/<id>
 @app.route('/api/server/<server_id>', methods=['PUT'])
 def api_put_server_id(server_id):
-    pass
+    assert server_id in app.config['SERVERS']
+    assert request.content_type == 'application/json'
+    data = json.loads(request.data)
+    assert 'url' in data
+    app.config['SERVERS'][server_id] = data['url']
 
-##      DELETE /server/<id>
-##          Delete a server connection
+## DELETE /server/<id>
+## Delete a server connection
 @app.route('/api/server/<server_id>', methods=['DELETE'])
 def api_delete_server_id(server_id):
-    pass
+    assert server_id in app.config['SERVERS']
+    del app.config['SERVERS'][server_id]
 
-##      GET /server/<id>/message
-##          List messages on a screen
+## GET /server/<id>/message
+## List messages on a screen
 @app.route('/api/server/<server_id>/message', methods=['GET'])
 def api_get_server_id_message(server_id):
-    pass
+    client = get_server_connection(server_id)
+    return _json_response(client.list_messages())
 
-##      POST /server/<id>/message
-##          Add a message to a screen
+## POST /server/<id>/message
+## Add a message to a screen
 @app.route('/api/server/<server_id>/message', methods=['POST'])
 def api_post_server_id_message(server_id):
-    pass
+    assert request.content_type == 'application/json'
+    data = json.loads(request.data)
+    client = get_server_connection(server_id)
+    return _json_response(client.add_message(data['text'], color=data.get('color')))
 
-##      GET /server/<id>/message/<id>
-##          Get a specific message from a screen
+## GET /server/<id>/message/<id>
+## Get a specific message from a screen
 @app.route('/api/server/<server_id>/message/<message_id>', methods=['GET'])
 def api_get_server_id_message_id(server_id, message_id):
-    pass
+    client = get_server_connection(server_id)
+    return _json_response(client.get_message(message_id))
 
-##      PUT /server/<id>/message/<id>
+## PUT /server/<id>/message/<id>
 @app.route('/api/server/<server_id>/message/<message_id>', methods=['PUT'])
 def api_put_server_id_message_id(server_id, message_id):
-    pass
+    client = get_server_connection(server_id)
+    pass  # todo: implement the update method
 
-##      DELETE /server/<id>/message/<id>
+## DELETE /server/<id>/message/<id>
 @app.route('/api/server/<server_id>/message/<message_id>', methods=['DELETE'])
 def api_delete_server_id_message_id(server_id, message_id):
-    pass
+    client = get_server_connection(server_id)
+    client.delete_message(message_id)
 
-##      GET /queue
+## GET /queue
 @app.route('/api/queue', methods=['GET'])
 def api_get_queue():
-    pass
+    pass  # todo: write this
 
-##      POST /queue
-##          Add something to the queue (useful?)
+## POST /queue
+## Add something to the queue (useful?)
 @app.route('/api/queue', methods=['POST'])
 def api_post_queue():
-    pass
+    pass  # todo: write this
 
-##      GET /queue/<id>
+## GET /queue/<id>
 @app.route('/api/queue/<queue_id>', methods=['GET'])
 def api_get_queue_id(queue_id):
-    pass
+    pass  # todo: write this
 
-##      PUT /queue/<id>
-##          Perform an action on a queue item (including approval)
+## PUT /queue/<id>
+## Perform an action on a queue item (including approval)
 @app.route('/api/queue/<queue_id>', methods=['PUT'])
 def api_put_queue_id(queue_id):
-    pass
+    pass  # todo: write this
 
-##      DELETE /queue/<id>
-##          Delete an item from the queue
+## DELETE /queue/<id>
+## Delete an item from the queue
 @app.route('/api/queue/<queue_id>', methods=['DELETE'])
 def api_delete_queue_id(queue_id):
-    pass
+    pass  # todo: write this
--- spotted_director/static/js/application.js
+++ spotted_director/static/js/application.js
@@ -3,56 +3,392 @@ $(function(){
     var container = $('#application-container');
 
 
+    // === Utilities ===========================================================
+
+    var Template = function(name) {
+        return Handlebars.compile($("#template-" + name).html());
+    };
+
+    var fetchModel = function(model, id, options) {
+        return new model({id: id}).fetch(options);
+    };
+
+    var updateActiveLinks = function(container, fragment) {
+        if (!fragment) {
+            fragment = Backbone.history.fragment;
+        }
+        if (!container) {
+            container = $(document.body);
+        }
+        $(container).find('.nav > li > a').each(function(){
+            var $this = $(this),
+                $parent = $($this.parent());
+            if ($this.attr('href') == '#' + fragment) {
+                $parent.addClass('active');
+            }
+            else {
+                $parent.removeClass('active');
+            }
+        });
+    };
+
+
+    // === Models ==============================================================
+
+    /**
+     * Screen server model
+     */
+    var ScreenServerModel = Backbone.Model.extend({
+        urlRoot: '/api/server',
+        defaults: {
+            id: null,
+            url: null
+        },
+        initialize: function(){
+            this.messages = new ScreenMessageCollection();
+            this.messages.parent = this;
+        },
+        getServerInfo: function(options) {
+            var url = this.urlRoot + '/' + this.get('id') + '/status';
+            $.ajax({
+                url: url,
+                dataType:"json",
+                success: options.success,
+                error: options.error
+            });
+        }
+    });
+
+    /**
+     * Message on a screen server
+     */
+    var ScreenMessageModel = Backbone.Model.extend({
+        defaults: {
+            id: null,
+            server: null, // Must be a valid ScreenServerModel id
+            text: "",
+            color: null
+        }
+    });
+
+    /**
+     * Message in the queue
+     */
+    var QueuedMessageModel = Backbone.Model.extend({
+        urlRoot: '/api/queue',
+        default: {
+            // ... something ...
+            id: null,
+            text: "",
+            color: null
+        }
+    });
+
+
+    // === Collections =========================================================
+
+    /**
+     * Collection to manage screen servers
+     * @type {*}
+     */
+    var ScreenServersCollection = Backbone.Collection.extend({
+        model: ScreenServerModel,
+        url: '/api/server'
+    });
+
+    /**
+     * Sub-collection of the screen message model
+     */
+    var ScreenMessageCollection = Backbone.Collection.extend({
+        model: ScreenMessageModel,
+        url: function(){
+            return '/api/server/' + this.parent.get('id') + '/message';
+        }
+    });
+
+    /**
+     * Collection to manage queued messages
+     */
+    var QueueCollection = Backbone.Collection.extend({
+        model: QueuedMessageModel,
+        url: '/api/queue'
+    });
+
+
+    // === Router ==============================================================
+
     var AppRouter = Backbone.Router.extend({
+        app: null, // reference to application
 
         routes: {
-            "":                 "index",    // #help
-            "help":                 "help",    // #help
-            "search/:query":        "search",  // #search/kiwis
-            "search/:query/p:page": "search"   // #search/kiwis/p7
+            "": "server_list",
+            "server/:server_id": "server_info",
+            "server/:server_id/messages": "server_messages",
+            "server/:server_id/queue": "server_queue",
+            "server/:server_id/archive": "server_archive",
+            "*path": "notFound"   // Handler for "404"
         },
 
-        index: function() {
-            container.html('Hello, index');
+        server_list: function() {
+            this.app.selectView('server_list');
         },
 
-        help: function() {
-            container.html('Help');
+        server_info: function(server_id) {
+            fetchModel(ScreenServerModel, server_id, {
+                success: function(model){
+                    this.app
+                        .selectView('server', {model: model})
+                        .selectView('info');
+                },
+                error: function(){
+                    this.app.selectView(
+                        'error', {message: "Unable to fetch model"});
+                }
+            });
         },
 
-        search: function(query, page) {
-            container.html('Search ' + query);
+        server_messages: function(server_id) {
+            fetchModel(ScreenServerModel, server_id, {
+                success:function(model){
+                    this.app
+                        .selectView('server', {model: model})
+                        .selectView('messages');
+                },
+                error: function(){
+                    this.app.selectView(
+                        'error', {message: "Unable to fetch model"});
+                }
+            });
+        },
+
+        server_queue: function(server_id) {
+            fetchModel(ScreenServerModel, server_id, {
+                success:function(model){
+                    this.app
+                        .selectView('server', {model: model})
+                        .selectView('queue');
+                },
+                error: function(){
+                    this.app.selectView(
+                        'error', {message: "Unable to fetch model"});
+                }
+            });
+        },
+
+        server_archive: function(server_id) {
+            fetchModel(ScreenServerModel, server_id, {
+                success:function(model){
+                    this.app
+                        .selectView('server', {model: model})
+                        .selectView('archive');
+                },
+                error: function(){
+                    this.app.selectView(
+                        'error', {message: "Unable to fetch model"});
+                }
+            });
+        },
+
+        notFound: function(path) {
+            container.html(Template('not-found')({path: path}));
         }
 
     });
 
 
-    var HomeView = Backbone.View.extend({
+    // === Views ===============================================================
 
-        initialize:function () {
-        },
+    /**
+     * View switcher
+     * @param parent
+     * @param container
+     * @param views
+     * @constructor
+     */
+    var ViewSwitcher = function(parent, container, views) {
+        this.parent = parent;
+        this.container = container;
+        this.subviews = views;
+        this.subview = null;
+        this.cached_views = {};
+    };
+    /**
+     * Switch the current view
+     * @param name
+     * @param args
+     */
+    ViewSwitcher.prototype.selectView = function(name, args) {
+        if (!args) {
+            // This works only for views without arguments
+            var cached_view;
+            if (cached_view = this.cached_views[name]) {
+                this.subview = cached_view;
+                this.subview.parent = this.parent;
+                this.subview.render();
+                this.container.html(this.subview.el);
+                return this.subview;
+            }
+        }
 
-        events:{
-            "click #showMeBtn":"showMeBtnClick"
-        },
+        this.subview = new this.subviews[name](args);
 
-        render:function () {
-            $(this.el).html(this.template());
+        if (!args) {
+            this.cached_views[name] = this.subview;
+        }
+
+        this.subview.parent = this.parent;
+        this.subview.render();
+        this.container.html(this.subview.el);
+        return this.subview;
+    };
+
+    /**
+     * Display an error message
+     */
+    var ErrorView = Backbone.View.extend({
+        render: function() {
+            var template = Template('error-message'),
+                message = this.options['message'];
+            this.$el.html(template({message:message}));
+        }
+    });
+
+    /**
+     * Main index: list of all the servers
+     */
+    var ServerListView = Backbone.View.extend({
+        render: function () {
+            var view = this;
+            view.$el.html('Loading...');
+            view.parent.collections.screen_servers.fetch({
+                success: function(collection, response, options) {
+                    var template = Template('server-list');
+                    view.$el.html(template({servers: response}));
+                },
+                error: function() {
+                    view.parent.selectView(
+                        'error', {message: "Error fetching servers list."});
+                }
+            });
             return this;
-        },
-
-        showMeBtnClick:function () {
-            app.headerView.search();
         }
+    });
 
+    /**
+     * Base for the "Screen" views
+     * @param model: The associated model (required!)
+     */
+    var ScreenServerBaseView = Backbone.View.extend({
+        initialize: function() {
+            var self = this;
+            this.switcher_el = $('<div>');
+            this.switcher = new ViewSwitcher(self, this.switcher_el, {
+                info: ScreenServerInfoView,
+                messages: ScreenServerMessagesView,
+                queue: null,
+                archive: null,
+                error: ErrorView
+            });
+        },
+        selectView: function(name, args) {
+            args = args || {};
+            args.model = this.model;
+            return this.switcher.selectView(name, args);
+        },
+        render: function() {
+            var template_h = Template('server-page-header'),
+                json_model = this.model.toJSON();
+            this.$el
+                .html(template_h({server: json_model}))
+                .append(this.switcher_el);
+            updateActiveLinks(this.$el);
+        }
+    });
+
+    /**
+     * Information about the server.
+     */
+    var ScreenServerInfoView = Backbone.View.extend({
+        render: function() {
+            var view = this;
+            view.$el.html('Loading...');
+
+            var json_model = this.model.toJSON();
+            var template = Template('server-dashboard');
+
+            this.model.getServerInfo({
+                success:function(data){
+                    view.$el.html(template({
+                        server: json_model,
+                        info: data
+                    }));
+                },
+                error: function(){
+                    view.parent.selectView('error', {message:'Unable to load server info.'})
+                }
+            });
+        }
+    });
+
+    /**
+     * List of messages currently displayed on the server.
+     * @type {*}
+     */
+    var ScreenServerMessagesView = Backbone.View.extend({
+        render: function() {
+            var view = this;
+            view.$el.html('Loading...');
+            this.model.messages.fetch({
+                success: function(collection, response, options) {
+
+                    var messages = [], json_message;
+                    collection.forEach(function(message){
+                        json_message = message.toJSON();
+                        json_message.html_color = json_message.color.slice(0, -2); // Remove alpha
+                        var shown_at = new Date(json_message._shown_at * 1000);
+                        json_message.shown_at_time = shown_at.toTimeString().split(' ')[0];
+                        json_message.shown_at = shown_at;
+
+                        json_message.shown_time = Math.round(json_message._shown_time);
+                        json_message.max_show_time = Math.round(json_message._max_show_time);
+
+                        messages.push(json_message);
+                    });
+
+                    var template = Template('server-messages');
+                    view.$el.html(template({messages:messages}));
+
+                },
+                error: function(){
+                    view.parent.selectView('error', {message:'Unable to load messages.'});
+                    //view.parent.parent.updateActiveLinks();
+                }
+            });
+
+        }
     });
 
 
+    // === Application =========================================================
 
-    var Application = function() {
+    var Application = function(container) {
         var self = this;
-        self.router = new AppRouter();
+        self.container = container;
 
+        self.switcher = new ViewSwitcher(self, self.container, {
+            server_list: ServerListView,
+            server: ScreenServerBaseView,
+            error: ErrorView
+        });
+
+        self.collections = {
+            screen_servers: new ScreenServersCollection()
+        };
+
+        self.router = new AppRouter();
+        self.router.app = self; // Need to reference..
+
+        // Handle internal links..
         $('body').on('click', 'a', function(e){
             var href = $(this).attr('href');
             if (href[0] == '#') {
@@ -62,27 +398,20 @@ $(function(){
             }
         });
 
-/*        $('a').on('click', function(){
-            var href = $(this).attr('href');
-            if (href[0] == '#') {
-                href = href.slice(1);
-                self.router.navigate(href, {trigger: true});
-            }
-            else if (href == '/') {
-                self.router.navigate(href, {trigger: true});
-            }
-        });*/
         Backbone.history.start({pushState: true});
     };
 
 
     Application.prototype = {
+
         navigate: function(destination) {
+            var self=this;
             this.router.navigate(destination, {"trigger": true});
-            this.updateActiveLinks();
+            self.updateActiveLinks();
         },
+
         updateActiveLinks: function() {
-            var frag = Backbone.history.fragment;
+            /*var frag = Backbone.history.fragment;
             $('.nav > li > a').each(function(){
                 var $this = $(this),
                     $parent = $($this.parent());
@@ -92,11 +421,16 @@ $(function(){
                 else {
                     $parent.removeClass('active');
                 }
-            });
+            });*/
+            updateActiveLinks();
+        },
+
+        selectView: function(view_name, args) {
+            return this.switcher.selectView(view_name, args);
         }
     };
 
-    window.app = new Application();
+    window.app = new Application(container);
     app.updateActiveLinks();
 
 });
